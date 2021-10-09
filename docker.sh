8e0701c78dfb604caeb0b7954de29a5e7f705890
slurm <ryan@fannon.ent>
2021-10-08 20:45:00 -0600

    Updated docker shell

--- docker.sh
+++ docker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo docker build -t king-bot:dev .
-sudo rm kingbot.tar
-sudo docker save -o kingbot.tar king-bot
-sudo chmod -R 777 kingbot.tar
+sudo docker build -t king-bot:latest .
+sudo docker push slurmking/king-bot
 
